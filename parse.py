f504d25a6b8736675e5e24207c14589209ffcc5e
Praneet <praneetkahndelwal1996@gmail.com>
2018-04-27 21:50:39 +0530

    Modular python code

--- parse.py
+++ parse.py
@@ -1,27 +1,7 @@
 import xml.etree.ElementTree as ET
-tree = ET.parse('cos.xml')
-cs_root = tree.getroot()
-courses = {}
-course_prereq_dict = {}
-course_overlap_dict = {}
-for course in cs_root:
-	pre_req = course.find('course_prereq')
-	overlap = course.find('course_overlap')
-	course_id = course.attrib['id']
-	courses[course_id] = 1
-	course_prereq_dict[course_id] = []
-	course_overlap_dict[course_id] = []
 
-	 
-	for pre_reqs in pre_req.findall('ir'):
-		course_prereq_dict[course_id].append(pre_reqs.attrib['refid'])
-
-	for overlaps in overlap.findall('ir'):
-		course_overlap_dict[course_id].append(overlaps.attrib['refid'])
-	
-print len(course_prereq_dict),len(course_overlap_dict)	
-
-def add_course(course_id):
+# Function for adding a new course in the list of courses
+def add_course(course_id, root):
 	new_course = ET.Element("course")
 	new_course.set('id',course_id)
 	new_element = ET.Element("course_no")
@@ -36,23 +16,10 @@ def add_course(course_id):
 	ET.SubElement(new_course,"course_prereq")
 	ET.SubElement(new_course,"course_overlap")
 	ET.SubElement(new_course,"course_content")
-	cs_root.append(new_course)
+	root.append(new_course)
+	return root
 
-for course in course_prereq_dict:
-	for prereq_course in course_prereq_dict[course]:
-		if prereq_course not in courses:
-			courses[prereq_course] = 1
-			add_course(prereq_course)
-			print prereq_course, course_prereq_dict[course]
-
-for course in course_overlap_dict:
-	for overlap_course in course_overlap_dict[course]:
-		if overlap_course not in courses:
-			courses[overlap_course] = 1
-			add_course(overlap_course)
-			print overlap_course, course_overlap_dict[course]
-
-
+# Function for indenting the entire xml file in a readable format
 def indent(elem, level=0):
     i = "\n" + level*"  "
     j = "\n" + (level-1)*"  "
@@ -70,7 +37,47 @@ def indent(elem, level=0):
             elem.tail = j
     return elem
 
-cs_root = indent(cs_root)
 
+def get_edges():
+	tree = ET.parse('cos.xml')
+	cs_root = tree.getroot()
+	courses = {}
+	course_prereq_dict = {}
+	course_overlap_dict = {}
+	for course in cs_root:
+		pre_req = course.find('course_prereq')
+		overlap = course.find('course_overlap')
+		course_id = course.attrib['id']
+		courses[course_id] = 1
+		course_prereq_dict[course_id] = []
+		course_overlap_dict[course_id] = []		 
+		for pre_reqs in pre_req.findall('ir'):
+			course_prereq_dict[course_id].append(pre_reqs.attrib['refid'])
+	
+		for overlaps in overlap.findall('ir'):
+			course_overlap_dict[course_id].append(overlaps.attrib['refid'])
+		
+	print len(course_prereq_dict),len(course_overlap_dict)	
+	# Dictionary for pre requisite and overlapping courses created 
 
-tree.write("cos.xml",encoding='UTF-8', xml_declaration=True)
+	# This part finds and adds those courses which are not already present in the courses list
+	for course in course_prereq_dict:
+		for prereq_course in course_prereq_dict[course]:
+			if prereq_course not in courses:
+				courses[prereq_course] = 1
+				cs_root = add_course(prereq_course,cs_root)
+				print prereq_course, course_prereq_dict[course]
+	
+	for course in course_overlap_dict:
+		for overlap_course in course_overlap_dict[course]:
+			if overlap_course not in courses:
+				courses[overlap_course] = 1
+				cs_root = add_course(overlap_course,cs_root)
+				print overlap_course, course_overlap_dict[course]
+	
+	# Update the xml file with the new courses
+	cs_root = indent(cs_root)
+	tree.write("cos.xml",encoding='UTF-8', xml_declaration=True)
+	return course_prereq_dict, course_overlap_dict
+
+course_prereq_dict, course_overlap_dict = get_edges()
